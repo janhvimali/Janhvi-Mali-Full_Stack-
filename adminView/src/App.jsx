071c44ddd657da24dcd1880bfcf43e44fc85fef3
Janhvi <janhvimali222@gmail.com>
2025-01-17 22:39:33 +0530

    Changes

--- adminView/src/App.jsx
+++ adminView/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('https://backend-kappa-umber.vercel.app');
+const socket = io('https://backend-kappa-umber.vercel.app:3000');
 
 function AdminView() {
   const [score, setScore] = useState({
@@ -14,7 +14,7 @@ function AdminView() {
   });
 
   useEffect(() => {
-    fetch('https://backend-kappa-umber.vercel.app/score')
+    fetch('https://backend-kappa-umber.vercel.app:3000/score')
       .then((res) => res.json())
       .then((data) => setScore(data));
 
@@ -26,7 +26,7 @@ function AdminView() {
   }, []);
 
   const handleUpdate = (runs, out = false) => {
-    fetch('https://backend-kappa-umber.vercel.app/score', {
+    fetch('https://backend-kappa-umber.vercel.app:3000/score', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ action: out ? { out: true } : { runs } }),
